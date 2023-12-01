72913eee4f9b9dedb920c4d5d7f8d79df854bb4b
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-12-01 23:13:32 +0900

    서브트리 수 구하기 - 정답 코드

--- 37.cpp
+++ 37.cpp
@@ -6,41 +6,45 @@
 using namespace std;
 
 //dfs
-int dfs(vector<vector<int> >& v, stack<int> stack, bool visit[]){
+int dfs(vector<vector<int>>& v, stack<int> stack, bool visit[]){
     int subNodes = 1; //node 수 초기화
     while(!stack.empty()){
         int curN = stack.top(); stack.pop();
         int nums = v[curN][0]; //인접노드 수
         for(int i=1; i<=nums; i++){
-            int adjN = v[curN][i];
-            if(!visit[adjN]){ //방문 안 한 애
-                stack.push(adjN); //stack에 인접 노드 추가
-                subNodes++;
-            }
-            visit[adjN] = true; //미래에 방문할 것 방문 표시
+            stack.push(v[curN][i]); //stack에 인접 노드 추가
+            visit[i] = true; //방문 표시
+            subNodes++;
         }
     }
     return subNodes;
 }
 
 int main (void){
-    int testcase = 1;
+    int testcase; cin >> testcase;
     for(int tc=0; tc<testcase; tc++){
-        vector<vector<int> > vec = {
-            {},
-            {1, 3},
-            {1, 3},
-            {3, 1, 2, 7},
-            {0},
-            {1, 6},
-            {1, 5},
-            {3, 3, 8, 9},
-            {2, 7, 9},
-            {2, 7, 8}
-        };
-        int n = 9;
-
+        vector<vector<int> > v;
+        vector<int> v2; v.push_back(v2); //0번째는 빈 배열
+        
+        int n; cin >> n;
+        for(int i=0; i<n; i++){
+            int idx, nums; cin >> idx >> nums;
+            vector<int> v2;
+            v2.push_back(nums);
+            for(int j=1; j<=nums; j++){
+                int node; cin >> node;
+                v2.push_back(node);
+            }
+            v.push_back(v2);
+        }
         // v 완성.
+        // cout << "완성" << endl;
+        // for(int i=0; i<v.size(); i++) {
+        //     for(int j=0; j<v[i].size(); j++){
+        //         cout << v[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
         bool visit[n+1]; //visit 배열 만들기
         for(int i=1; i<=n; i++) visit[i] = false;
 
@@ -50,7 +54,7 @@ int main (void){
         for(int i=1; i<=n; i++){
             if(!visit[i]){ // 방문 안 함
                 stack.push(i); visit[i] = true;
-                int subNodes = dfs(vec, stack, visit);
+                int subNodes = dfs(v, stack, visit);
                 subG.push_back(subNodes); //정답 기록
             }
         }
@@ -64,3 +68,4 @@ int main (void){
     }
     return 0;
 }
+//0일때 출력만 살짝 관리
